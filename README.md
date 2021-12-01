c7a42558478fa12b9611f88bd4b51201f02f8f9c
Artemis21 <artemisdev21@gmail.com>
2021-12-01 05:17:40 +0000

    Add day 1 solutions.

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ Get the session token cookie from the advent of code website (using whatever too
 provides to inspect cookies). Place that in `src/aoc_helper/.token`.
 
 Once installed, you can use `python3.10 -m src --help` to see the available options.
+
+## My Scores :)
+
+```
+      -------Part 1--------   -------Part 2--------
+Day       Time  Rank  Score       Time  Rank  Score
+  1   00:00:51    46     55   00:05:27   747      0
+```
--- src/day_01.py
+++ src/day_01.py
@@ -9,12 +9,16 @@ class Day(Solution):
 
     def __init__(self, raw: str):
         """Parse and store the raw data."""
+        self.nums = list(map(int, raw.splitlines()))
 
     def part_1(self) -> str | int | None:
         """Calculate the answer for part 1."""
+        return sum(self.nums[i + 1] > self.nums[i] for i in range(len(self.nums) - 1))
 
     def part_2(self) -> str | int | None:
         """Calculate the answer for part 2."""
+        sums = [sum(self.nums[i:i + 3]) for i in range(len(self.nums) - 2)]
+        return sum(sums[i + 1] > sums[i] for i in range(len(sums) - 1))
 
 
 if __name__ == "__main__":
